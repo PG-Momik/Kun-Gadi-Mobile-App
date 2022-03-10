b6b7c877084ac8c35c8494646c9626137b77f5cf
shreejupradhan <shreejupradhan39@gmail.com>
2022-03-11 00:05:11 +0545

    Add data model- ConNodeInfo.java

---
+++ java/com/example/sixth/Models/ConNodeInfo.java
@@ -0,0 +1,94 @@
+package com.example.sixth.Models;
+
+public class ConNodeInfo {
+    private String id;
+    private String n_id;
+    private String user_id;
+    private String state_id;
+    private String name;
+    private String n_lat;
+    private String n_lng;
+    private String o_lat;
+    private String o_lng;
+    private String created;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getN_id() {
+        return n_id;
+    }
+
+    public void setN_id(String n_id) {
+        this.n_id = n_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(String state_id) {
+        this.state_id = state_id;
+    }
+
+    public String getN_lat() {
+        return n_lat;
+    }
+
+    public void setN_lat(String n_lat) {
+        this.n_lat = n_lat;
+    }
+
+    public String getN_lng() {
+        return n_lng;
+    }
+
+    public void setN_lng(String n_lng) {
+        this.n_lng = n_lng;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getO_lat() {
+        return o_lat;
+    }
+
+    public void setO_lat(String o_lat) {
+        this.o_lat = o_lat;
+    }
+
+    public String getO_lng() {
+        return o_lng;
+    }
+
+    public void setO_lng(String o_lng) {
+        this.o_lng = o_lng;
+    }
+}
