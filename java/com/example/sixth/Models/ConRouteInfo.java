d7615e6a8070c1b26f1a83752eddbfbbdcf4cd54
shreejupradhan <shreejupradhan39@gmail.com>
2022-03-20 00:00:00 +0545

    Add data model- ConRouteInfo.java

---
+++ java/com/example/sixth/Models/ConRouteInfo.java
@@ -0,0 +1,85 @@
+package com.example.sixth.Models;
+
+public class ConRouteInfo {
+    private String id;
+    private String route_id;
+    private String user_id;
+    private String state_id;
+    private String created;
+    private String path;
+    private String actual_path;
+    private String start;
+    private String end;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoute_id() {
+        return route_id;
+    }
+
+    public void setRoute_id(String route_id) {
+        this.route_id = route_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(String state_id) {
+        this.state_id = state_id;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getActual_path() {
+        return actual_path;
+    }
+
+    public void setActual_path(String actual_path) {
+        this.actual_path = actual_path;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
