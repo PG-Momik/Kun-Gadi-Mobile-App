c0cb8bd43bdff05d595aff87396d9ef72025cab2
shreejupradhan <shreejupradhan39@gmail.com>
2022-03-21 00:00:00 +0545

    Add data model- NodeInfo.java

---
+++ java/com/example/sixth/Models/NodeInfo.java
@@ -0,0 +1,41 @@
+package com.example.sixth.Models;
+
+public class NodeInfo {
+    private String id;
+    private String name;
+    private String longitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    private String latitude;
+}
