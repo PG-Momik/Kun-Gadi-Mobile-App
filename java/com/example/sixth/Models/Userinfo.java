2f24e63fca7daf7316a219f08c673cce92acb082
shreejupradhan <shreejupradhan39@gmail.com>
2022-03-23 00:00:00 +0545

    Add data model- Userinfo.java

---
+++ java/com/example/sixth/Models/Userinfo.java
@@ -0,0 +1,72 @@
+package com.example.sixth.Models;
+
+public class Userinfo {
+    private String id, username, password, conPassword, email, phone, roll, roll_id;
+
+    public String getConPassword() {
+        return conPassword;
+    }
+
+    public void setConPassword(String conPassword) {
+        this.conPassword = conPassword;
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public String getRoll_id() {
+        return roll_id;
+    }
+
+    public void setRoll_id(String roll_id) {
+        this.roll_id = roll_id;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
