844bd5ba5cc1c494734dc61c500df52b9f467781
shreejupradhan <shreejupradhan39@gmail.com>
2022-03-27 00:05:49 +0545

    Finish Activity- AboutUsActivity.java

---
+++ java/com/example/sixth/Activities/AboutUsActivity.java
@@ -0,0 +1,18 @@
+package com.example.sixth.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sixth.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+
+}
